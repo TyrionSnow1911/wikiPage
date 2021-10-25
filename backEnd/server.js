811c4d58fda27312653a8af3524461d8a4e8658e
TyrionSnow1911 <glm3531@gmail.com>
2021-10-25 15:57:07 -0600

    WIP: add routes to server.js

--- backEnd/server.js
+++ backEnd/server.js
@@ -2,16 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/articles/", (req, res) => {
-  res.send("");
+app.get("/articles/", (req, response) => {
+  response.send("");
 });
 
-app.put("/articles/:name", (req, res) => {
-  res.send("");
+app.put("/articles/:name", (req, response) => {
+  response.send("");
 });
 
-app.get("/articles/:name", (req, res) => {
-  res.send("");
+app.get("/articles/:name", (req, response) => {
+  response.send("");
 });
 
 app.listen(port, () =>
